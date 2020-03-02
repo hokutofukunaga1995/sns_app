d11ee94eed41d0b361c61c982e66cc61f8c3f9c8
FukunagaHokuto <h5355@icloud.com>
2020-03-02 19:50:00 +0900

    [add] files

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import BoardModel
 
 # Create your views here.
 def signup_func(request):
@@ -23,11 +26,24 @@ def login_func(request):
             user = authenticate(request, username = username_input, password = password_input)
             if user is not None:
                 login(request, user)
-                return redirect('signup')
+                return redirect('list')
             else:
                 return redirect('login')
         return render(request, 'login.html')
 
-    
+
+@login_required
 def list_func(request):
-    return render(request, 'list.html')
+    object_list = BoardModel.objects.all()
+    if object_list.first() is None:
+        return render(request, 'list.html', {'error':'投稿はありません。'})
+    return render(request, 'list.html', {'object_list':object_list})
+
+def logout_func(request):
+    logout(request)
+    return redirect('login')
+
+
+def detail_func(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object':object})
